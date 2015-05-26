a677940b07b2d9254633915de538d1648dd4437b
kunsuoh <kunsuoh@gmail.com>
2015-05-27 07:48:36 +0900

    Cuts

--- StRoot/StPicoNpeAnaMaker/StCuts.h
+++ StRoot/StPicoNpeAnaMaker/StCuts.h
@@ -1,5 +1,5 @@
-#ifndef NPE_ANA_CUTS_H
-#define NPE_ANA_CUTS_H
+#ifndef CUTS_H
+#define CUTS_H
 /* **************************************************
  *  Cuts namespace.
  *
@@ -14,43 +14,62 @@
 #include "Rtypes.h"
 #include <string>
 
-namespace anaCuts
+namespace cutsAna
 {
     // path to lists of triggers prescales
     // lists are obtained from http://www.star.bnl.gov/protected/common/common2014/trigger2014/plots_au200gev/
-    // Below cuts are default value of StPicoNpeEnentMaker
-    
-    std::string const prescalesFilesDirectoryName = "./run14AuAu200GeVPrescales";
-    
-    //event
+
+    // event
     float const vz = 6.0;// cm.
     float const vzVpdVz = 3.0; // 3 cm.
+    unsigned char const trigger = 19; // 19: BHT1, 21: BHT2, 23: BHT3
+    unsigned char const triggerLength = 0x1F;
     
-    //tracking
-    int const nHitsFit = 15;
-    float const pt = .2;
+    // hadron selectron
+    float const nSigPion = 2;
+    bool const pionRequireHFT = true;
+    int const pionNHitsFit = 20;
+    int const pionNhitsDedx = 15;
+    float const pionHitRatio = 0.52;
+    float const pionEta = 0.7;
+    float const pionPt = 1.5;
     
-    // electrons
-    bool const requireHFT = true;
-    float const nSigmaElectron = 3.;
+    // electron
+    bool const electronRequireHFT = true;
+    int const electronNHitsFit = 20;
+    int const electronNhitsDedx = 15;
+    float const electronHitRatio = 0.52;
+    float const electronEta = 0.7;
+    float const electronPt = 1.5;
+    
     
     // partner
-    float const nSigmaPartnerElectron = 3.;
+    int const partnerNHitsFit = 15;
+    float const partnerEta = 1.;
+    float const partnerPt = 0.2;
     
-    // tree electron pair cuts
-    float const pairMass = 0.2;
-    float const pairMassHigh = 0.4;
-    float const pairHighPt = 2.;
-    float const pairDca = 1;
-    float const positionX = 200.;
-    float const positionY = 200.;
-    float const positionZ = 200.;
     
-    // histograms electron pair cuts
-    float const qaNHitsFit = 20;
-    float const qaNSigmaElectronMax = 3.0;
-    float const qaNSigmaElectronMin = -1.0;
-    float const qaPairDca = 0.1;
-    float const qaPairMass = 0.1;
+    // electron + partner pair cuts
+    float const pairMass = 0.4;
+    float const pairDca = 3.;
+    
+    
+    
+    // pure electron pair cut
+    float const pureElectronMass = 0.01;
+    float const pureElectronDca = 0.01;
+    
+    
+    // pid
+    float const nSigElectron = 3;
+    float const partnerNSigElectron = 13;
+    int const emcNEta = 0;//1;
+    int const emcNPhi = 0;//1;
+    float const emcEoverPLow = 0;//0.5;
+    float const emcEoverPHigh = 10;//1.7;
+    float const emcPhiDist = 10;//0.013;
+    float const emcZDist = 100;//2;
+    float const emcAssDist = 10;//0.05;
+    float const tofBeta = 1;//0.1;
 }
 #endif
