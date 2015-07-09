d9085f214363eec0e4efe3bfdbbda7aa505930ad
kunsuoh <kunsuoh@gmail.com>
2015-07-09 09:03:39 +0900

    fix bug

--- StRoot/StPicoNpeAnaMaker/StPicoNpeAnaMaker.cxx
+++ StRoot/StPicoNpeAnaMaker/StPicoNpeAnaMaker.cxx
@@ -166,7 +166,7 @@ Int_t StPicoNpeAnaMaker::Make()
     }
     int checkDoubleTrigger = 0;
     for (int i=0; i<25; i++) if (picoDst->event()->triggerWord() >> i & 0x1) {
-        cout << "Prescale (" << mPicoNpeEvent->runId() << ", " << i << ") : " << mPrescales->prescale(mPicoNpeEvent->runId(), i) << endl;
+        cout << "Prescale (" << mPicoNpeEvent->runId() << ", " << i << ", " << mPicoNpeEvent->eventId() << ") : " << mPrescales->prescale(mPicoNpeEvent->runId(), i) << endl;
         checkDoubleTrigger++;
     }
     if (checkDoubleTrigger > 1) {
