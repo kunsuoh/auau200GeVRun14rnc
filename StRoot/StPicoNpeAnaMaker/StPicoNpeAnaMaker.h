792d046cb1a5001a02f279d5d56eb66e5ea269bc
kunsuoh <kunsuoh@gmail.com>
2015-08-05 01:52:45 +0900

    Fix bug

--- StRoot/StPicoNpeAnaMaker/StPicoNpeAnaMaker.h
+++ StRoot/StPicoNpeAnaMaker/StPicoNpeAnaMaker.h
@@ -144,7 +144,7 @@ class StPicoNpeAnaMaker : public StMaker
 
     TH1F * hRefMult[5];
     TH1F * hRefMultWt[5];
-    TH1F * histo[10][10][10][10];
+    TH1F * histo[10][10][10][11];
     TH1F * histoTofMass[10];
     TH1F * histoNSigE[10][10];
     int nptbin;
