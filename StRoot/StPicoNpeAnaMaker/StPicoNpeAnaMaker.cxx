fe5c9b541cd50654b6553ce24399d46c14a9e2c2
kunsuoh <kunsuoh@gmail.com>
2015-08-07 12:48:09 +0900

    check bug

--- StRoot/StPicoNpeAnaMaker/StPicoNpeAnaMaker.cxx
+++ StRoot/StPicoNpeAnaMaker/StPicoNpeAnaMaker.cxx
@@ -183,7 +183,12 @@ Int_t StPicoNpeAnaMaker::Make()
     }
     hCheckDoubleTrigger->Fill(checkDoubleTrigger);
     hCheckDoubleTrigger->Fill(checkDoubleTrigger18);
-    
+
+    ////////////////////////////////////////////////////
+    weight = mPrescales->prescale(mPicoNpeEvent->runId(), 1);
+    int i=1;
+    cout << "Prescale (" << mPicoNpeEvent->runId() << ", " << i << ", " << mPicoNpeEvent->eventId() << ") : " << mPrescales->prescale(mPicoNpeEvent->runId(), i) << endl;
+
     
     if (picoDst->event()->triggerWord()>>0 & 0x3) isHTEvents = 1;
     else return kStOK;
@@ -191,7 +196,7 @@ Int_t StPicoNpeAnaMaker::Make()
     weight = mPrescales->prescale(mPicoNpeEvent->runId(), 1);
     int i=1;
     cout << "Prescale (" << mPicoNpeEvent->runId() << ", " << i << ", " << mPicoNpeEvent->eventId() << ") : " << mPrescales->prescale(mPicoNpeEvent->runId(), i) << endl;
-    
+    /////////////////////////////////////////////////////
     
     hEvent->Fill(5);
     hEvent->Fill(5,weight);
