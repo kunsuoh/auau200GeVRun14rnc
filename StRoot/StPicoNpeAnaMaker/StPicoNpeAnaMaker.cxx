1efe36956d8909cba88e3e1a79740b0a775e4f07
kunsuoh <kunsuoh@gmail.com>
2015-08-10 18:52:21 +0900

    fix bug

--- StRoot/StPicoNpeAnaMaker/StPicoNpeAnaMaker.cxx
+++ StRoot/StPicoNpeAnaMaker/StPicoNpeAnaMaker.cxx
@@ -252,7 +252,7 @@ Int_t StPicoNpeAnaMaker::Make()
                     setVariables(epair);
                     if (pairCharge == 0) fillHistogram(0); // US
                     else fillHistogram(1);                 // LS
-                    cout << "1" ;
+                    cout << " 1 " << pairCharge ;
                 }
             } // .. end make electron pairs
         } // .. end of tagged e loop
@@ -269,7 +269,7 @@ Int_t StPicoNpeAnaMaker::Make()
                 setVariables(epair);
                 if (pairCharge == 0) fillHistogram(0); // US
                 else fillHistogram(1);                 // LS
-                cout << "0";
+                cout << " 0 " << pairCharge;
             }
         }
   //  }
