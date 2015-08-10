6c86c5c50e58378a10bd5ed241ac64a75b25d4cc
kunsuoh <kunsuoh@gmail.com>
2015-08-10 18:57:36 +0900

    print eta, pt

--- StRoot/StPicoNpeAnaMaker/StPicoNpeAnaMaker.cxx
+++ StRoot/StPicoNpeAnaMaker/StPicoNpeAnaMaker.cxx
@@ -252,7 +252,7 @@ Int_t StPicoNpeAnaMaker::Make()
                     setVariables(epair);
                     if (pairCharge == 0) fillHistogram(0); // US
                     else fillHistogram(1);                 // LS
-                    cout << " 1-" << pairMass << "," <<pairDca ;
+                    cout << " 1-" << pairMass << "," <<pairDca << "," << pt << "," << eta ;
                 }
             } // .. end make electron pairs
         } // .. end of tagged e loop
@@ -269,7 +269,7 @@ Int_t StPicoNpeAnaMaker::Make()
                 setVariables(epair);
                 if (pairCharge == 0) fillHistogram(0); // US
                 else fillHistogram(1);                 // LS
-                cout << " 0-" << pairMass << "," <<pairDca ;
+                cout << " 0-" << pairMass << "," << pairDca << "," << pt << "," << eta ;
             }
         }
   //  }
