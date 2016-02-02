faef7b207315cac9ad021a5a9cc8f6724b898df2
kunsuoh <kunsuoh@gmail.com>
2016-02-02 14:54:07 -0800

    default to minSize

--- readMcPicoDst/StRoot/StPicoMcNpeAnaMaker/StElectronPair.cxx
+++ readMcPicoDst/StRoot/StPicoMcNpeAnaMaker/StElectronPair.cxx
@@ -75,7 +75,7 @@ mLength(std::numeric_limits<float>::quiet_NaN())
     StPhysicalHelixD partnerHelix = partner->dcaGeometry().helix();
     
     // normal method
-    pair<double,double> ss = electronHelix.pathLengths(partnerHelix, 100000);
+    pair<double,double> ss = electronHelix.pathLengths(partnerHelix);
     StThreeVectorD kAtDcaToPartner = electronHelix.at(ss.first);
     StThreeVectorD pAtDcaToElectron = partnerHelix.at(ss.second);
     
