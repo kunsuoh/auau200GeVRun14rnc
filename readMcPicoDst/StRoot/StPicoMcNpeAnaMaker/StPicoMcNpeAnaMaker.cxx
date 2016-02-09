1b0f93c410da13367d6c1ee260be49973bdc5d78
kunsuoh <kunsuoh@gmail.com>
2016-02-09 13:02:22 -0800

    rm delete ..

--- readMcPicoDst/StRoot/StPicoMcNpeAnaMaker/StPicoMcNpeAnaMaker.cxx
+++ readMcPicoDst/StRoot/StPicoMcNpeAnaMaker/StPicoMcNpeAnaMaker.cxx
@@ -216,7 +216,7 @@ Int_t StPicoMcNpeAnaMaker::Make()
                 StPicoMcTrack *mcParentTrk = (StPicoMcTrack*)picoDst->mctrack(mcTrk->parentId());
                 parentGid=mcParentTrk->GePid();
                 //if(mcParentTrk->parentId() != Pico::USHORTMAX) continue;
-                delete mcParentTrk;
+                //delete mcParentTrk;
             }
             trackId=mcTrk->GePid();
             hTrackParentGeantId->Fill(parentGid);
