188101c59182866ce9fbf3352d838528d389a63a
kunsuoh <kunsuoh@gmail.com>
2016-02-19 16:54:54 -0800

    add McAna in chain

--- readMcPicoDst/work/Hijing_Pi0RealRaw/run.sh
+++ readMcPicoDst/work/Hijing_Pi0RealRaw/run.sh
@@ -16,14 +16,14 @@ at=`perl -e 'srand; print int(rand(99)+1)'`
 cp -p /star/data01/pwg/kunsu/pileup/pileupSet$at/pile**.root ./Files_$job/pile_up/pile_up$at.root
 
 # ---- Producing sim file .fzd
-root4star -b -l <<EOF
-//.L starsim.hijing.Pi0.C
-.L starsim.hijing.gamma.C
-starsim(1,$run,$RANDOM)
-.q
-EOF
+#root4star -b -l <<EOF
+#//.L starsim.hijing.Pi0.C
+#.L starsim.hijing.gamma.C
+#starsim(1,$run,$RANDOM)
+#.q
+#EOF
 #mv hijing_pi0real* ./Files_$job/fzd/.
-mv gamma* ./Files_$job/fzd/.
+#mv gamma* ./Files_$job/fzd/.
 
 echo "Kunsu: HFT reco starting"
 # ---- HFT reconstruction
@@ -32,7 +32,7 @@ end=19
 #inFile=Files_$job/fzd/hijing_pi0real_$run.starsim.fzd
 inFile=Files_$job/fzd/gamma_$run.starsim.fzd
 inPile=Files_$job/pile_up/pile_up$at.root
-chain=y2014a,event,McEvent,MuDst,tpc,fzin,sim_T,gen_T,geantout,tpcrs,TpcHitMover,TpxClu,evout,-HitFilt,FieldOn,AgML,usexgeom,MakeEvent,ITTF,Sti,NoSsdIt,NoSvtIt,StiHftC,pxlFastSim,pxlCluster,pxlHit,istFastSim,Idst,BAna,l0,Tree,logger,genvtx,tpcDB,bbcSim,btofsim,tags,emcY2,EEfs,evout,-dstout,IdTruth,big,McEvout,MiniMcMk,StiPulls,ReadAll,clearmem
+chain=y2014a,event,McEvent,MuDst,tpc,fzin,sim_T,gen_T,geantout,tpcrs,TpcHitMover,TpxClu,evout,-HitFilt,FieldOn,AgML,usexgeom,MakeEvent,ITTF,Sti,NoSsdIt,NoSvtIt,StiHftC,pxlFastSim,pxlCluster,pxlHit,istFastSim,Idst,BAna,l0,Tree,logger,genvtx,tpcDB,bbcSim,btofsim,tags,emcY2,EEfs,evout,-dstout,IdTruth,big,McEvout,MiniMcMk,StiPulls,ReadAll,clearmem,McAna
 echo $chain
 
 pwd 
