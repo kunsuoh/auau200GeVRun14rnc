8ee153f361191fca0da47c76905327627812e779
kunsuoh <kunsuoh@gmail.com>
2016-02-23 01:46:20 -0800

    test alsdjfao;isdgnfawoisfd

--- readMcPicoDst/work/Hijing_Pi0RealRaw/StRoot/StMcAnalysisMaker/StMcAnalysisMaker.cxx
+++ readMcPicoDst/work/Hijing_Pi0RealRaw/StRoot/StMcAnalysisMaker/StMcAnalysisMaker.cxx
@@ -163,47 +163,6 @@ int StMcAnalysisMaker::fillTracks(StMcEvent* mcEvent,StEvent* event)
     }
     
     
-    std::vector<Int_t> idMcElectrons;
-    std::vector<Int_t> idMcPositrons;
-
-    for (unsigned int i = 0;  i < trks.size(); i++)
-    {
-        StMcTrack* mcTrack = trks[i];
-        Int_t trackGid = mcTrack->geantId();
-        
-        if (trackGid==1 && mcTrack->stopVertex()) {
-            cout << " !!!!!!!!!!!!!! numberOfDaughters: " << mcTrack->stopVertex()->numberOfDaughters();
-            for (int j = 0; j < mcTrack->stopVertex()->numberOfDaughters(); j++){
-                cout << " " << mcTrack->stopVertex()->daughter(j)->pt() << " " << mcTrack->stopVertex()->daughter(j)->geantId();
-            }
-            cout << endl;
-        }
-        
-        
-        continue;
-        
-        
-        if (trackGid!=2 && trackGid!=3 ) continue;
-
-        int ncommonhits = 0;
-        StTrack const* rcTrack = findPartner(mcTrack, ncommonhits);
-        if(rcTrack)
-        {
-            if(StGlobalTrack const* glRcTrack = dynamic_cast<StGlobalTrack const*>(rcTrack))
-            {
-                
-                if (trackGid==2) {
-                    idMcPositrons.push_back(i);
-                }
-                else if (trackGid==3){
-                    idMcElectrons.push_back(i);
-                }
-            }
-        }
-    }
-
-    cout << ">>>>>>>>>>>>>>>>>>>>>>>> " << trks.size() << " " << idMcPositrons.size() << " " << idMcElectrons.size() << endl;
-
     nPair=0;
     nMcPxl1Hits=0;
     nMcPxl2Hits=0;
@@ -212,216 +171,95 @@ int StMcAnalysisMaker::fillTracks(StMcEvent* mcEvent,StEvent* event)
     nRcPxl2Hits=0;
     nRcIstHits=0;
     
-    for(int i = 0; i < idMcPositrons.size(); i++){
-        StMcTrack * positron = trks[i];
-        StMcTrack * parentPositron = (StMcTrack *)positron->parent();
-        if (!parentPositron) continue;
-        for(int j = 0; j < idMcElectrons.size(); j++){
-            StMcTrack * electron = trks[j];
-            StMcTrack * parentElectron = (StMcTrack *)electron->parent();
-            if (!parentElectron) continue;
-            //if (parentPositron!=parentElectron) continue;
-
-            cout << i << " " << j << endl;
-            
-            
-            pairPt[nPair] = parentPositron->pt();
-            pairEta[nPair] = parentPositron->pseudoRapidity();
-            openangle[nPair] = 0;
-            mcopenangle[nPair] = positron->momentum().angle(electron->momentum());
-            mcDist_pxl1[nPair] = 0;
-            mcDist_pxl2[nPair] = 0;
-            mcDist_ist[nPair] = 0;
-            rcDist_pxl1[nPair] = 0;
-            rcDist_pxl2[nPair] = 0;
-            rcDist_ist[nPair] = 0;
-            convR[nPair] = TMath::Sqrt(electron->startVertex()->position().x()*electron->startVertex()->position().x()+electron->startVertex()->position().y()*electron->startVertex()->position().y());
-            parentGid[nPair] = parentPositron->geantId();
-            mass[nPair] = 0;
-            pairDca[nPair] = 0;
-            pt1[nPair] = positron->pt();
-            pt2[nPair] = electron->pt();
-            eta1[nPair] = positron->pseudoRapidity();
-            eta2[nPair] = electron->pseudoRapidity();
-            clusterSize1_pxl1[nPair] = 0;
-            clusterSize1_pxl2[nPair] = 0;
-            clusterSize2_pxl1[nPair] = 0;
-            clusterSize2_pxl2[nPair] = 0;
-            idTruth[nPair] = 1; // electron(2) or positron(1) idTruth
-            rcHftHit1_pxl1[nPair]=0;
-            rcHftHit1_pxl2[nPair]=0;
-            rcHftHit1_ist[nPair]=0;
-            truth1_pxl1[nPair]=0;
-            truth1_pxl2[nPair]=0;
-            truth1_ist[nPair]=0;
-            nHits1_pxl1[nPair]=0;
-            nHits1_pxl2[nPair]=0;
-            nHits1_ist[nPair]=0;
-            rcHftHit2_pxl1[nPair]=0;
-            rcHftHit2_pxl2[nPair]=0;
-            rcHftHit2_ist[nPair]=0;
-            truth2_pxl1[nPair]=0;
-            truth2_pxl2[nPair]=0;
-            truth2_ist[nPair]=0;
-            nHits2_pxl1[nPair]=0;
-            nHits2_pxl2[nPair]=0;
-            nHits2_ist[nPair]=0;
-
-            
-            if (1) nMcPxl1Hits++;
-            if (1) nMcPxl2Hits++;
-            if (1) nMcIstHits++;
-            if (1) nRcPxl1Hits++;
-            if (1) nRcPxl2Hits++;
-            if (1) nRcIstHits++;
-            nPair++;
+    for (unsigned int i = 0;  i < trks.size(); i++){
+        StMcTrack* mcTrack = trks[i];
+        Int_t trackGid = mcTrack->geantId();
+        if (trackGid==1 && mcTrack->stopVertex()) {
+            StMcTrack * positron = 0;
+            StMcTrack * electron = 0;
+            StGlobalTrack * rcPositron = 0;
+            StGlobalTrack * rcElectron = 0;
+            for (int j = 0; j < mcTrack->stopVertex()->numberOfDaughters(); j++){
+                StMcTrack * dauTrack = mcTrack->stopVertex()->daughter(j);
+                Int_t dauTrackGid = dauTrack->geantId();
+                
+                if (dauTrackGid!=2 && dauTrackGid!=3 ) continue;
+                
+                int ncommonhits = 0;
+                StTrack const* rcTrack = findPartner(dauTrack, ncommonhits);
+                if(rcTrack)
+                {
+                    if(StGlobalTrack const* glRcTrack = dynamic_cast<StGlobalTrack const*>(rcTrack))
+                    {
+                        if (dauTrackGid==2) {
+                            StMcTrack * positron = dauTrack;
+                            StGlobalTrack * rcPositron = glRcTrack;
+                        }
+                        else if (dauTrackGid==3) {
+                            StMcTrack * electron = dauTrack;
+                            StGlobalTrack * rcElectron = glRcTrack;
+                        }
+                    }
+                }
+            }// end dauther loop
+            if (rcElectron && rcPositron && electron && positron) {
+                pairPt[nPair] = mcTrack->pt();
+                pairEta[nPair] = mcTrack->pseudoRapidity();
+                openangle[nPair] = 0;
+                mcopenangle[nPair] = positron->momentum().angle(electron->momentum());
+                mcDist_pxl1[nPair] = 0;
+                mcDist_pxl2[nPair] = 0;
+                mcDist_ist[nPair] = 0;
+                rcDist_pxl1[nPair] = 0;
+                rcDist_pxl2[nPair] = 0;
+                rcDist_ist[nPair] = 0;
+                convR[nPair] = TMath::Sqrt(electron->startVertex()->position().x()*electron->startVertex()->position().x()+electron->startVertex()->position().y()*electron->startVertex()->position().y());
+                parentGid[nPair] = parentPositron->geantId();
+                mass[nPair] = 0;
+                pairDca[nPair] = 0;
+                pt1[nPair] = positron->pt();
+                pt2[nPair] = electron->pt();
+                eta1[nPair] = positron->pseudoRapidity();
+                eta2[nPair] = electron->pseudoRapidity();
+                clusterSize1_pxl1[nPair] = 0;
+                clusterSize1_pxl2[nPair] = 0;
+                clusterSize2_pxl1[nPair] = 0;
+                clusterSize2_pxl2[nPair] = 0;
+                idTruth[nPair] = 1; // electron(2) or positron(1) idTruth
+                rcHftHit1_pxl1[nPair]=0;
+                rcHftHit1_pxl2[nPair]=0;
+                rcHftHit1_ist[nPair]=0;
+                truth1_pxl1[nPair]=0;
+                truth1_pxl2[nPair]=0;
+                truth1_ist[nPair]=0;
+                nHits1_pxl1[nPair]=0;
+                nHits1_pxl2[nPair]=0;
+                nHits1_ist[nPair]=0;
+                rcHftHit2_pxl1[nPair]=0;
+                rcHftHit2_pxl2[nPair]=0;
+                rcHftHit2_ist[nPair]=0;
+                truth2_pxl1[nPair]=0;
+                truth2_pxl2[nPair]=0;
+                truth2_ist[nPair]=0;
+                nHits2_pxl1[nPair]=0;
+                nHits2_pxl2[nPair]=0;
+                nHits2_ist[nPair]=0;
+                
+                
+                if (1) nMcPxl1Hits++;
+                if (1) nMcPxl2Hits++;
+                if (1) nMcIstHits++;
+                if (1) nRcPxl1Hits++;
+                if (1) nRcPxl2Hits++;
+                if (1) nRcIstHits++;
+                nPair++;
+            }
         }
     }
     mTree->Fill();
     
     return kStOk;
 }
-/*
- 
- float pTrkSvx, pTrkSvy;
-        if(mcTrack->startVertex())
-        {
-            pTrkSvx = mcTrack->startVertex()->position().x();
-            pTrkSvy = mcTrack->startVertex()->position().y();
-        }
-        else continue;
-        
-        if(sqrt(pTrkSvx*pTrkSvx + pTrkSvy*pTrkSvy)>3.) continue;
-        
-        int ncommonhits = 0;
-        StTrack const* rcTrack = findPartner(mcTrack, ncommonhits);
-        
-        bool istTruth = true;
-        bool pxlTruth1 = true;
-        bool pxlTruth2 = true;
-        
-        if (rcTrack)
-        {
-            StPtrVecHit rcIstHits = rcTrack->detectorInfo()->hits(kIstId);
-            StPtrVecHit rcPxlHits = rcTrack->detectorInfo()->hits(kPxlId);
-            
-            int nRcIstHits = (int)rcIstHits.size();
-            int nRcPxlHits = (int)rcPxlHits.size();
-            
-            
-            for (int iHit = 0; iHit < nRcIstHits; ++iHit)
-            {
-                if (rcIstHits[iHit]->idTruth() != mcTrack->key())
-                {
-                    istTruth = false;
-                    break;
-                }
-            }
-            
-            for (int iHit = 0; iHit < nRcPxlHits; ++iHit)
-            {
-                if (rcPxlHits[iHit]->idTruth() != mcTrack->key())
-                {
-                    StThreeVectorF pos = rcPxlHits[iHit]->position();
-                    
-                    float const R = pow(pos.x(), 2.0) + pow(pos.y(), 2.0);
-                    if (R > 3.5 * 3.5) pxlTruth2 = false;
-                    else pxlTruth1 = false;
-                }
-            }
-        }
-        
-        float dca = -999.;
-        float dcaXY = -999.;
-        float dcaZ = -999.;
-        
-        if(rcTrack)
-        {
-            if(StGlobalTrack const* glRcTrack = dynamic_cast<StGlobalTrack const*>(rcTrack))
-            {
-                if(glRcTrack->dcaGeometry())
-                {
-                    StPhysicalHelixD helix = glRcTrack->dcaGeometry()->helix();
-                    dca = helix.distance(event->primaryVertex()->position());
-                    dcaXY = helix.geometricSignedDistance(event->primaryVertex()->position().x(),event->primaryVertex()->position().y());
-                    
-                    StThreeVectorF dcaPoint = helix.at(helix.pathLength(event->primaryVertex()->position().x(),event->primaryVertex()->position().y()));
-                    dcaZ = dcaPoint.z() - event->primaryVertex()->position().z();
-                }
-            }
-        }
-
-        
-        mTree->Fill();
-        
-        continue; // this is a hack
-        if (!rcTrack) continue;
-        
-        StPtrVecHit rcTpcHits = rcTrack->detectorInfo()->hits(kTpcId);
-        // const StPtrVecMcHit* mcTpcHits = mcTrack->Hits(kTpcId);
-        
-        StPhysicalHelixD helix = rcTrack->geometry()->helix();
-        
-        for (size_t ih = 0; ih < rcTpcHits.size(); ih++)
-        {
-            StTpcHit* rcHit = dynamic_cast<StTpcHit*>(rcTpcHits[ih]);
-            if (!rcHit) continue;
-            
-            pair<rcTpcHitMapIter, rcTpcHitMapIter>  bounds = mAssoc->rcTpcHitMap()->equal_range(rcHit);
-            
-            // loop over all mcHits associated with this rcHit
-            bool found = false;
-            for (rcTpcHitMapIter iter = bounds.first; iter != bounds.second; iter++)
-            {
-                const StMcTpcHit* mcHit = (*iter).second;
-                
-                // fill histograms if this mcHit belongs to this mcTrack
-                if (mcHit->parentTrack()->key() == mcTrack->key())
-                {
-                    StThreeVectorF mcHitTotRcTrack = helix.at(helix.pathLength(mcHit->position().x(),mcHit->position().y())) - mcHit->position();
-                    
-                    
-                    
-                    mTree->Fill();
-                    
-                    found = true;
-                }
-                else
-                {
-                    cout << "Not a good candidate" << endl;
-                }
-            }
-            
-            if (!found)
-            {
-                cout << "No mc hit was found for this rc Hit!!!!" << endl;
-            }
-        }
-        
-        
-         cout<<"ncommonhits "<<ncommonhits<<endl;
-         cout<<"Rc track hits"<<endl;
-         for(size_t ih = 0; ih <rcTpcHits.size(); ih++)
-         {
-         StTpcHit* rcHit = dynamic_cast<StTpcHit*>(rcTpcHits[ih]);
-         if(!rcHit) continue;
-         cout<<rcHit->qaTruth()<<endl;
-         }
-         
-         cout<<"MC track hits"<<endl;
-         for(size_t ih = 0; ih <mcTpcHits->size(); ih++)
-         {
-         StMcTpcHit* mcHit = dynamic_cast<StMcTpcHit*>((*mcTpcHits)[ih]);
-         if(!mcHit) continue;
-         cout<<mcHit->key()<<endl;
-         }
- 
-    }
-    
-    return kStOk;
-}
-
-*/
 //________________________________________________
 const StTrack* StMcAnalysisMaker::findPartner(StMcTrack* mcTrack, int& maxCommonTpcHits)
 {
