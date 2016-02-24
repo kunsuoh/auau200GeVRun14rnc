91837b3a9cc05c432b00d0bef8bf44a340592a77
kunsuoh <kunsuoh@gmail.com>
2016-02-24 14:05:52 -0800

    change run.sh file

--- readMcPicoDst/work/Hijing_Pi0RealRaw/run.sh
+++ readMcPicoDst/work/Hijing_Pi0RealRaw/run.sh
@@ -18,22 +18,22 @@ mkdir ./Files_$job/picodst
 
 
 # ---- Producing sim file .fzd
-root4star -b -l <<EOF
-//.L starsim.hijing.Pi0.C
-.L starsim.hijing.gamma.C
-starsim(4,$run,$RANDOM)
-.q
-EOF
-mv hijing_pi0real* ./Files_$job/fzd/.
-mv gamma* ./Files_$job/fzd/.
+#root4star -b -l <<EOF
+#//.L starsim.hijing.Pi0.C
+#.L starsim.hijing.gamma.C
+#starsim(4,$run,$RANDOM)
+#.q
+#EOF
+#mv hijing_pi0real* ./Files_$job/fzd/.
+#mv gamma* ./Files_$job/fzd/.
 
 echo "Kunsu: HFT reco starting"
 # ---- HFT reconstruction
 start=0
 end=19
 #inFile=Files_$job/fzd/hijing_pi0real_$run.starsim.fzd
-inFile=Files_$job/fzd/gamma_$run.starsim.fzd
-#inFile=/star/u/kunsu/pwg/gamma/fz/gamma_$run.fzd
+#inFile=Files_$job/fzd/gamma_$run.starsim.fzd
+inFile=/star/u/kunsu/pwg/Pi0Raw/fz/gamma_$run.fzd
 inPile=Files_$job/pile_up/pile_up$at.root
 chain=y2014a,event,McEvent,MuDst,tpc,fzin,sim_T,gen_T,geantout,tpcrs,TpcHitMover,TpxClu,evout,-HitFilt,FieldOn,AgML,usexgeom,MakeEvent,ITTF,Sti,NoSsdIt,NoSvtIt,StiHftC,pxlFastSim,pxlCluster,pxlHit,istFastSim,Idst,BAna,l0,Tree,logger,genvtx,tpcDB,bbcSim,btofsim,tags,emcY2,EEfs,evout,-dstout,IdTruth,big,McEvout,MiniMcMk,StiPulls,ReadAll,clearmem,McAna
 echo $chain
