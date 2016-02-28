ad07d673474152beb0e31593c8313cc3c5693410
kunsuoh <kunsuoh@gmail.com>
2016-02-27 23:28:44 -0800

    if..

--- readMcPicoDst/work/Hijing_Pi0RealRaw/run.sh
+++ readMcPicoDst/work/Hijing_Pi0RealRaw/run.sh
@@ -5,19 +5,30 @@ job=$1
 #run=${job: -1}
 run=`echo $job | cut -f2 -d"_"`
 
+makeFolder=0
+makeFZ=0
+makeReco=0
+makeRecoPileup=0
+makePico=0
+makeZip=0
+makeQa=0
 # ---- Make folder
+if [ $makeFolder -eq 1 ]; then
 mkdir ./Files_$job
 mkdir ./Files_$job/fzd
 mkdir ./Files_$job/hft_reco
 mkdir ./Files_$job/pile_up
 mkdir ./Files_$job/picodst
-
+fi
 # ---- Pile up file
-#at=`perl -e 'srand; print int(rand(99)+1)'`
-#cp -p /star/data01/pwg/kunsu/pileup/pileupSet$at/pile**.root ./Files_$job/pile_up/pile_up$at.root
-
+if [ $makeRecoPileup -eq 1 ]; then
+at=`perl -e 'srand; print int(rand(99)+1)'`
+cp -p /star/data01/pwg/kunsu/pileup/pileupSet$at/pile**.root ./Files_$job/pile_up/pile_up$at.root
+fi
 
 # ---- Producing sim file .fzd
+if [ $makeFZ -eq 1 ]; then
+echo "Kunsu: make sim file .fzd"
 root4star -b -l <<EOF
 .L starsim.hijing.gamma.C
 starsim(9,$run,$RANDOM)
@@ -25,7 +36,9 @@ starsim(9,$run,$RANDOM)
 EOF
 
 mv pi0Dalitz_* ./Files_$job/fzd/.
+fi
 
+if [ $makeReco -eq 1 ]; then
 echo "Kunsu: HFT reco starting"
 # ---- HFT reconstruction
 start=0
@@ -35,7 +48,11 @@ inFile=Files_$job/fzd/pi0Dalitz_$run.starsim.fzd
 #inFile=/star/u/kunsu/pwg/Pi0Raw/fz/gamma_$run.fzd   # pi0 real
 #inFile=/star/u/kunsu/pwg/gamma/fz/gamma_$run.fzd    # gamma flat pt
 inPile=Files_$job/pile_up/pile_up$at.root
+if [ $makeQa -eq 1 ]; then
 chain=y2014a,event,McEvent,MuDst,tpc,fzin,sim_T,gen_T,geantout,tpcrs,TpcHitMover,TpxClu,evout,-HitFilt,FieldOn,AgML,usexgeom,MakeEvent,ITTF,Sti,NoSsdIt,NoSvtIt,StiHftC,pxlFastSim,pxlCluster,pxlHit,istFastSim,Idst,BAna,l0,Tree,logger,genvtx,tpcDB,bbcSim,btofsim,tags,emcY2,EEfs,evout,-dstout,IdTruth,big,McEvout,MiniMcMk,StiPulls,ReadAll,clearmem,McAna
+else
+chain=y2014a,event,McEvent,MuDst,tpc,fzin,sim_T,gen_T,geantout,tpcrs,TpcHitMover,TpxClu,evout,-HitFilt,FieldOn,AgML,usexgeom,MakeEvent,ITTF,Sti,NoSsdIt,NoSvtIt,StiHftC,pxlFastSim,pxlCluster,pxlHit,istFastSim,Idst,BAna,l0,Tree,logger,genvtx,tpcDB,bbcSim,btofsim,tags,emcY2,EEfs,evout,-dstout,IdTruth,big,McEvout,MiniMcMk,StiPulls,ReadAll,clearmem
+fi
 echo $chain
 
 pwd 
@@ -47,12 +64,14 @@ pxl->useIdealGeom(); // ideal geometry
 //pxl->useDbGeom();  // survey geometry
 pxl->useRandomSeed();
 
-//pxl->addPileup();
-//pxl->setPileupFile("$inPile");
+if [ $makeRecoPileup -eq 1 ]; then
+pxl->addPileup();
+pxl->setPileupFile("$inPile");
+fi
 
-
+if [ $makeQa -eq 1 ]; then
 StMcAnalysisMaker* mcAnalysisMaker = (StMcAnalysisMaker*)chain->GetMaker("StMcAnalysisMaker");
-
+fi
 
 chain->Init();
 chain->EventLoop($start,$end);
@@ -62,12 +81,15 @@ EOF
 
 mv pi0Dalitz_*.root Files_$job/hft_reco/.
 mv mcAnalysis.pxlSimQa.root Files_$job/hft_reco/.
+fi
 
+if [ $makePico -eq 1 ]; then
 # ---- PicoDst
 #root4star -l -b -q makePicoDst.C\($run,\"Files_$job/hft_reco/hijing_pi0real_$run.MuDst.root\",\"Files_$job/hft_reco/hijing_pi0real_$run.McEvent.root\"\)
 #root4star -l -b -q makePicoDst.C\($run,\"Files_$job/hft_reco/pi0Dalitz_$run.MuDst.root\",\"Files_$job/hft_reco/pi0Dalitz_$run.McEvent.root\"\)
 #mv *.picoDst.root Pi0Raw_hijing_sim_production_v0_$job.picoDst.root
 #mv *.picoDst.root sim_production_v0_$job.picoDst.root
+fi
 
 #privilges
 find Files_$job/ -type d -exec chgrp rhstar {} \;
@@ -76,5 +98,10 @@ find Files_$job/ -type f -exec chgrp rhstar {} \;
 find Files_$job/ -type f -exec chmod g+rw {} \;
 
 # ---- Done bring files back
+if [ $makeZip -eq 1 ]; then
 tar -cvf Files_$job.tar Files_$job
+fi
+
+if [ $makeQa -eq 1 ]; then
 mv Files_$job/hft_reco/mcAnalysis.pxlSimQa.root /star/u/kunsu/pwg/Pi0Dalitz/qaHist/qa_$job.root
+fi
