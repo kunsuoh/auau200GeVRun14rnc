cdedb12ff6cc5c9441fa8237375361313be40763
kunsuoh <kunsuoh@gmail.com>
2016-02-27 23:38:17 -0800

    test run

--- readMcPicoDst/work/Hijing_Pi0RealRaw/run.sh
+++ readMcPicoDst/work/Hijing_Pi0RealRaw/run.sh
@@ -7,11 +7,12 @@ run=`echo $job | cut -f2 -d"_"`
 
 makeFolder=1
 makeFZ=0
-makeReco=0
+makeReco=1
 makeRecoPileup=0
 makePico=0
 makeZip=0
-makeQa=0
+makeQa=1
+inputSource="gamma"
 
 # ---- Make folder
 if [ $makeFolder -eq 1 ]; then
@@ -22,6 +23,7 @@ mkdir ./Files_$job/hft_reco
 mkdir ./Files_$job/pile_up
 mkdir ./Files_$job/picodst
 fi
+
 # ---- Pile up file
 if [ $makeRecoPileup -eq 1 ]; then
 at=`perl -e 'srand; print int(rand(99)+1)'`
@@ -32,12 +34,12 @@ fi
 if [ $makeFZ -eq 1 ]; then
 echo "Kunsu: make sim file .fzd"
 root4star -b -l <<EOF
-.L starsim.hijing.gamma.C
+.L starsim.hijing.$inputSource.C
 starsim(9,$run,$RANDOM)
 .q
 EOF
 
-mv pi0Dalitz_* ./Files_$job/fzd/.
+mv $inputSource_* ./Files_$job/fzd/.
 fi
 
 if [ $makeReco -eq 1 ]; then
@@ -45,10 +47,12 @@ echo "Kunsu: HFT reco starting"
 # ---- HFT reconstruction
 start=0
 end=19
-#inFile=Files_$job/fzd/hijing_pi0real_$run.starsim.fzd
 inFile=Files_$job/fzd/pi0Dalitz_$run.starsim.fzd
-#inFile=/star/u/kunsu/pwg/Pi0Raw/fz/gamma_$run.fzd   # pi0 real
-#inFile=/star/u/kunsu/pwg/gamma/fz/gamma_$run.fzd    # gamma flat pt
+if [ $makeFZ -eq 1 ]; then
+inFile=Files_$job/fzd/pi0Dalitz_$run.starsim.fzd
+else
+inFile=/star/u/kunsu/pwg/$inputSource/fz/$inputSource$run.fzd
+fi
 inPile=Files_$job/pile_up/pile_up$at.root
 if [ $makeQa -eq 1 ]; then
 chain=y2014a,event,McEvent,MuDst,tpc,fzin,sim_T,gen_T,geantout,tpcrs,TpcHitMover,TpxClu,evout,-HitFilt,FieldOn,AgML,usexgeom,MakeEvent,ITTF,Sti,NoSsdIt,NoSvtIt,StiHftC,pxlFastSim,pxlCluster,pxlHit,istFastSim,Idst,BAna,l0,Tree,logger,genvtx,tpcDB,bbcSim,btofsim,tags,emcY2,EEfs,evout,-dstout,IdTruth,big,McEvout,MiniMcMk,StiPulls,ReadAll,clearmem,McAna
@@ -81,16 +85,16 @@ chain->Finish();
 
 EOF
 
-mv pi0Dalitz_*.root Files_$job/hft_reco/.
+mv $inputSource_*.root Files_$job/hft_reco/.
+if [ $makeQa -eq 1 ]; then
 mv mcAnalysis.pxlSimQa.root Files_$job/hft_reco/.
+fi
 fi
 
 if [ $makePico -eq 1 ]; then
 # ---- PicoDst
-#root4star -l -b -q makePicoDst.C\($run,\"Files_$job/hft_reco/hijing_pi0real_$run.MuDst.root\",\"Files_$job/hft_reco/hijing_pi0real_$run.McEvent.root\"\)
-root4star -l -b -q makePicoDst.C\($run,\"Files_$job/hft_reco/pi0Dalitz_$run.MuDst.root\",\"Files_$job/hft_reco/pi0Dalitz_$run.McEvent.root\"\)
-#mv *.picoDst.root Pi0Raw_hijing_sim_production_v0_$job.picoDst.root
-mv *.picoDst.root sim_production_v0_$job.picoDst.root
+root4star -l -b -q makePicoDst.C\($run,\"Files_$job/hft_reco/$inputSource_$run.MuDst.root\",\"Files_$job/hft_reco/$inputSource_$run.McEvent.root\"\)
+mv *.picoDst.root $inputSource_$job.picoDst.root
 fi
 
 #privilges
@@ -105,5 +109,5 @@ tar -cvf Files_$job.tar Files_$job
 fi
 
 if [ $makeQa -eq 1 ]; then
-mv Files_$job/hft_reco/mcAnalysis.pxlSimQa.root /star/u/kunsu/pwg/Pi0Dalitz/qaHist/qa_$job.root
+mv Files_$job/hft_reco/mcAnalysis.pxlSimQa.root /star/u/kunsu/pwg/$inputSource/qaHist/qa_$job.root
 fi
