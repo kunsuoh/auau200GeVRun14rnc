62624c9fcaac6d9273c59b9c0cc37a79386dac09
kunsuoh <kunsuoh@gmail.com>
2016-03-05 20:29:27 -0800

    wrongRow

--- readMcPicoDst/work/Hijing_Pi0RealRaw/submit.sh
+++ readMcPicoDst/work/Hijing_Pi0RealRaw/submit.sh
@@ -3,7 +3,8 @@ date
 particle=pi0real
 nrun=$1
 path=/star/data01/pwg/kunsu/$particle/
-star-submit-template -template submit.xml -entities nRuns=${nrun},base=${path},particle=${particle}
+tag=_wrongRow
+star-submit-template -template submit.xml -entities nRuns=${nrun},base=${path},particle=${particle},tag=${tag}
 #Fix privileges for report, log, etc.
 find ./ -user $USER -exec chgrp rhstar {} \;
 find ./ -user $USER -exec chmod g+rw {} \;
