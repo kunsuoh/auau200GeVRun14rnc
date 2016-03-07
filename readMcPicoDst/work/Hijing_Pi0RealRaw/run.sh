ee8b88b4009528f0831cf8874617d680625801f7
kunsuoh <kunsuoh@gmail.com>
2016-03-07 14:55:17 -0800

    test run

--- readMcPicoDst/work/Hijing_Pi0RealRaw/run.sh
+++ readMcPicoDst/work/Hijing_Pi0RealRaw/run.sh
@@ -7,7 +7,7 @@ run=`echo $job | cut -f2 -d"_"`
 inputSource=$2
 
 makeFolder=1
-makeReco=1
+makeReco=0
 makeRecoPileup=1
 makePico=1
 makeZip=0
@@ -45,7 +45,73 @@ cp Files_$job/fzd/* /star/u/kunsu/pwg/$inputSource/fz/
 
 fi
 
+# ---- TPC reconstruction
+if [ -s /star/u/kunsu/pwg/${inputSource}/tpc_reco/${inputSource}_${run}.event.root ]; then
+echo "Kunsu: Skip make tpc_reco files"
+else
+start=0
+end=19
+if [ -s /star/u/kunsu/pwg/${inputSource}/fz/${inputSource}_${run}.starsim.fzd ]; then
+inFile=/star/u/kunsu/pwg/${inputSource}/fz/${inputSource}_${run}.starsim.fzd
+else
+inFile=Files_$job/fzd/${inputSource}_$run.starsim.fzd
+fi
+chain=y2014a,event,tpc,fzin,geantout,tpcrs,TpcHitMover,TpxClu,evout,-HitFilt
+
+root4star -b -l <<EOF
+.x bfc.C($start,$end,"$chain","$inFile");
+EOF
+mv ${inputSource}_${run}* /star/u/kunsu/pwg/${inputSource}/tpc_reco/
+fi
+
 # ---- HFT reconstruction
+inFile2=/star/u/kunsu/pwg/${inputSource}/tpc_reco/${inputSource}_${run}.event.root
+inPile=Files_$job/pile_up/pile_up$at.root
+if [ $makeQa -eq 1 ]; then
+chain2=y2014a,event,McEvent,MuDst,in,sim_T,gen_T,geantout,evout,FieldOn,AgML,usexgeom,MakeEvent,ITTF,Sti,NoSsdIt,NoSvtIt,StiHftC,pxlFastSim,istFastSim,Idst,BAna,l0,Tree,logger,genvtx,tpcDB,bbcSim,btofsim,emcY2,EEfs,evout,-dstout,IdTruth,big,McEvout,MiniMcMk,McAna,ReadAll,clearmem,pxlCluster,pxlHit
+else
+chain2=y2014a,event,McEvent,MuDst,in,sim_T,gen_T,geantout,evout,FieldOn,AgML,usexgeom,MakeEvent,ITTF,Sti,NoSsdIt,NoSvtIt,StiHftC,pxlFastSim,istFastSim,Idst,BAna,l0,Tree,logger,genvtx,tpcDB,bbcSim,btofsim,emcY2,EEfs,evout,-dstout,IdTruth,big,McEvout,MiniMcMk,ReadAll,clearmem,pxlCluster,pxlHit
+fi
+echo $chain
+pwd
+#env
+if [ -s .temprun.sh ]; then
+rm .temprun.sh
+fi
+echo "starver SL15k" > .temprun.sh
+echo "root4star -b -l <<EOF" > .temprun.sh
+echo ".x bfc.C(-1,\"$chain2\",\"$inFile2\");" >> .temprun.sh
+echo "StPxlSimMaker* pxl = chain->GetMaker(\"pxlSimMaker\");" >> .temprun.sh
+#echo "pxl->useIdealGeom(); // ideal geometry" >> .temprun.sh
+echo "pxl->useDbGeom();  // survey geometry" >> .temprun.sh
+#echo "pxl->setFastSim();" >> .temprun.sh
+echo "pxl->setFastSimRaw();" >> .temprun.sh
+echo "pxl->setWrongRowRatio(0.5,0.5);" >> .temprun.sh # -1 : off, other : ratio
+echo "pxl->useRandomSeed();" >> .temprun.sh
+if [ $makeRecoPileup -eq 1 ]; then
+echo "pxl->addPileup();" >> .temprun.sh
+echo "pxl->setPileupFile(\"$inPile\");" >> .temprun.sh
+fi
+if [ $makeQa -eq 1 ]; then
+echo "StMcAnalysisMaker* mcAnalysisMaker = (StMcAnalysisMaker*)chain->GetMaker(\"StMcAnalysisMaker\");" >> .temprun.sh
+fi
+echo "chain->Init();" >> .temprun.sh
+echo "chain->EventLoop($start,$end);" >> .temprun.sh
+echo "chain->Finish();" >> .temprun.sh
+echo "EOF" >> .temprun.sh
+
+chmod +x .temprun.sh
+./.temprun.sh
+#rm .temprun.sh
+
+mv ${inputSource}_*.root Files_$job/hft_reco/.
+if [ $makeQa -eq 1 ]; then
+mv mcAnalysis.pxlSimQa.root Files_$job/hft_reco/.
+fi
+
+
+
+
 if [ $makeReco -eq 1 ]; then
     echo "Kunsu: HFT reco starting"
     start=0
