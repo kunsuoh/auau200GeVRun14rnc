25669f35fcdf269af9a9d900f4295f5becbaafac
kunsuoh <kunsuoh@gmail.com>
2016-03-08 07:33:20 -0800

    0.4,0.2

--- readMcPicoDst/work/Hijing_Pi0RealRaw/submit.sh
+++ readMcPicoDst/work/Hijing_Pi0RealRaw/submit.sh
@@ -3,7 +3,7 @@ date
 particle=pi0real
 nrun=$1
 path=/star/data01/pwg/kunsu/$particle/
-tag=_wrongRow_0.5_0
+tag=_wrongRow_0.4_0.2
 star-submit-template -template submit.xml -entities nRuns=${nrun},base=${path},particle=${particle},tag=${tag}
 #Fix privileges for report, log, etc.
 find ./ -user $USER -exec chgrp rhstar {} \;
